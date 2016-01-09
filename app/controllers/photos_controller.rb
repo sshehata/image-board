a246d988f13c228a11c68b3113e52332e518ea47
sshihata <samy.s.shihata@gmail.com>
2016-01-09 13:29:23 +0200

    added photo model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,14 @@
 class PhotosController < ApplicationController
   def index
   end
+
+  def load_images
+    @photos = Photo.order(:views).find_in_batches(start: params[:start], batch_size: 10)
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109101756) do
+ActiveRecord::Schema.define(version: 20160109104619) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "uploaded_on"
+    t.integer  "views"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
